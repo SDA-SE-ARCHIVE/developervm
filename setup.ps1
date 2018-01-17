7a06eb4ba601d79ea4074d0387033447692f3d76
Marco Albrecht <Marco.Albrecht@signal-iduna.de>
2018-01-17 10:05:25 +0100

    fixing broken link

--- setup.ps1
+++ setup.ps1
@@ -3,9 +3,9 @@ vagrant halt
 
 $wshShellObject = New-Object -com WScript.Shell
 $userProfileFolder = (get-childitem env:USERPROFILE).Value
-$wshShellLink = $wshShellObject.CreateShortcut($userProfileFolder+"\Desktop\DeveloperVM.lnk") 
+$wshShellLink = $wshShellObject.CreateShortcut($userProfileFolder+"\Desktop\DeveloperVM2.0.lnk")
 $wshShellLink.TargetPath = "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe"
-$wshShellLink.Arguments ="--startvm DevelopmentBox --fullscreen"
+$wshShellLink.Arguments ="--startvm DevelopmentBox2.0 --fullscreen"
 $wshShellLink.WindowStyle = 1
 $wshShellLink.IconLocation = "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" 
 $wshShellLink.Save()
