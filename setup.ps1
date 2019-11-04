02da8303aa34fb1428b3695ca41053940b0aaf87
Andre Höing <andre.hoeing@msg.group>
2019-11-04 15:08:51 +0100

    new base image, fixed creation

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,11 @@
+vagrant plugin install vagrant-vbguest
 vagrant up
 vagrant halt
 
 $wshShellObject = New-Object -com WScript.Shell
 $userProfileFolder = (get-childitem env:USERPROFILE).Value
 $wshShellLink = $wshShellObject.CreateShortcut($userProfileFolder+"\Desktop\SDASE-DeveloperVM.lnk")
-$wshShellLink.TargetPath = "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe"
+$wshShellLink.TargetPath = "C:\Program Files\Oracle\VirtualBox\VirtualBoxVM.exe"
 $wshShellLink.Arguments ="--startvm DevelopmentBox --fullscreen"
 $wshShellLink.WindowStyle = 1
 $wshShellLink.IconLocation = "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" 
